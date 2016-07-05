556f267bf502c7577cdd60fd6a025cadfe1b1b07
Lyudmil Latinov <llatinov@gmail.com>
2016-07-05 12:03:01 +0300

    Re-factor existing code.

--- src/main/java/com/automationrhapsody/reststub/resources/BookService.java
+++ src/main/java/com/automationrhapsody/reststub/resources/BookService.java
@@ -4,17 +4,15 @@ import com.automationrhapsody.reststub.data.Book;
 import com.automationrhapsody.reststub.persistence.BookDB;
 import com.codahale.metrics.annotation.Timed;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("/books")
 public class BookService {
-
-    public BookService() {
-    }
 
     @GET
     @Timed
--- src/main/java/com/automationrhapsody/reststub/resources/PersonService.java
+++ src/main/java/com/automationrhapsody/reststub/resources/PersonService.java
@@ -4,15 +4,18 @@ import com.automationrhapsody.reststub.data.Person;
 import com.automationrhapsody.reststub.persistence.PersonDB;
 import com.codahale.metrics.annotation.Timed;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/person")
 public class PersonService {
-
-    public PersonService() {
-    }
 
     @GET
     @Timed
