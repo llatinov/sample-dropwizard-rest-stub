1a091850c3836351847fca978bdfabd354b5cc8e
Lyudmil Latinov <llatinov@gmail.com>
2016-07-05 12:03:01 +0300

    Re-factor existing code.

--- src/main/java/com/automationrhapsody/reststub/persistence/BookDB.java
+++ src/main/java/com/automationrhapsody/reststub/persistence/BookDB.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookDB {
-    private static List<Book> books = new ArrayList<Book>();
+    private static final List<Book> BOOKS = new ArrayList<>();
 
     static {
-        books.add(new Book("1492251275", "The Purloined Letter", "Edgar Allen Poe"));
-        books.add(new Book("1495375749", "The black cat", "Edgar Allen Poe"));
-        books.add(new Book("1467746150", "The Raven", "Edgar Allen Poe"));
+        BOOKS.add(new Book("1492251275", "The Purloined Letter", "Edgar Allen Poe"));
+        BOOKS.add(new Book("1495375749", "The black cat", "Edgar Allen Poe"));
+        BOOKS.add(new Book("1467746150", "The Raven", "Edgar Allen Poe"));
     }
 
     public static List<Book> getAll() {
-        return books;
+        return BOOKS;
     }
 }
--- src/main/java/com/automationrhapsody/reststub/persistence/PersonDB.java
+++ src/main/java/com/automationrhapsody/reststub/persistence/PersonDB.java
@@ -8,45 +8,45 @@ import java.util.List;
 import java.util.Map;
 
 public class PersonDB {
-    private static Map<Integer, Person> persons = new HashMap<Integer, Person>();
+    private static final Map<Integer, Person> PERSONS = new HashMap<>();
 
     static {
-        persons.put(1, new Person(1, "FN1", "LN1", "email1@email.na"));
-        persons.put(2, new Person(2, "FN2", "LN2", "email2@email.na"));
-        persons.put(3, new Person(3, "FN3", "LN3", "email3@email.na"));
-        persons.put(4, new Person(4, "FN4", "LN4", "email4@email.na"));
+        PERSONS.put(1, new Person(1, "FN1", "LN1", "email1@email.na"));
+        PERSONS.put(2, new Person(2, "FN2", "LN2", "email2@email.na"));
+        PERSONS.put(3, new Person(3, "FN3", "LN3", "email3@email.na"));
+        PERSONS.put(4, new Person(4, "FN4", "LN4", "email4@email.na"));
     }
 
     public static Person getById(int id) {
-        return persons.get(id);
+        return PERSONS.get(id);
     }
 
     public static List<Person> getAll() {
         List<Person> result = new ArrayList<Person>();
-        for (Integer key : persons.keySet()) {
-            result.add(persons.get(key));
+        for (Integer key : PERSONS.keySet()) {
+            result.add(PERSONS.get(key));
         }
         return result;
     }
 
     public static int getCount() {
-        return persons.size();
+        return PERSONS.size();
     }
 
     public static void remove() {
-        if (!persons.keySet().isEmpty()) {
-            persons.remove(persons.keySet().toArray()[0]);
+        if (!PERSONS.keySet().isEmpty()) {
+            PERSONS.remove(PERSONS.keySet().toArray()[0]);
         }
     }
 
     public static String save(Person person) {
         String result = "";
-        if (persons.get(person.getId()) != null) {
+        if (PERSONS.get(person.getId()) != null) {
             result = "Updated Person with id=" + person.getId();
         } else {
             result = "Added Person with id=" + person.getId();
         }
-        persons.put(person.getId(), person);
+        PERSONS.put(person.getId(), person);
         return result;
     }
 }
